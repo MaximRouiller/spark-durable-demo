6ffc62008e2198f1660ce36ee5a78a1f58b195bc
Maxime Rouiller <maxime.rouiller@microsoft.com>
2021-07-19 10:51:02 -0400

    adding monitoring

--- GetAdvocateDetails/index.js
+++ GetAdvocateDetails/index.js
@@ -1,17 +1,29 @@
-﻿const fetch = require('node-fetch');
+﻿const fetch = require('node-fetch').default;
+let appInsights = require('applicationinsights');
+appInsights.setup().start();
 
 module.exports = async function (context) {
-    let response = await fetch(`https://api.github.com/users/${context.bindings.username}`, {
+    const time = Date.now();
+    const url = `https://api.github.com/users/${context.bindings.username}`;
+    let response = await fetch(url, {
         withCredentials: true,
         credentials: 'include',
         headers: {
             'Accept': 'application/vnd.github.v3+json',
             'User-Agent': 'AppName: 1.0',
-            'Authorization': `Token ${process.env['GitToken']}`
-        }
+            'Authorization': `Token ${process.env['GitToken']}`,
+        },
     });
-    if(response.ok)
-        return response.json();
-    else
-        return {};
+    var success = response.ok;
+    appInsights.defaultClient.trackDependency({
+        resultCode: response.status,
+        name: 'GitHub/Get User',
+        dependencyTypeName: 'HTTP',
+        data: url,
+        duration: Date.now() - time,
+        success: success,
+    });
+
+    if (success) return response.json();
+    else return {};
 };
--- GetAdvocatesList/index.js
+++ GetAdvocatesList/index.js
@@ -1,14 +1,28 @@
-﻿const fetch = require('node-fetch');
+﻿const fetch = require('node-fetch').default;
+let appInsights = require('applicationinsights');
+appInsights.setup().start();
 
 module.exports = async function (context) {
+    const time = Date.now();
     let response = await fetch(process.env['AdvocatesAPI'], {
         withCredentials: true,
         credentials: 'include',
         headers: {
             'Accept': 'application/vnd.github.v3+json',
             'User-Agent': 'AppName: 1.0',
-            'Authorization': `Token ${process.env['GitToken']}`
-        }
+            'Authorization': `Token ${process.env['GitToken']}`,
+        },
     });
-    return response.json();
+    var success = response.ok;
+    appInsights.defaultClient.trackDependency({
+        resultCode: response.status,
+        name: 'API/Get Advocates',
+        dependencyTypeName: 'HTTP',
+        data: process.env['AdvocatesAPI'],
+        duration: Date.now() - time,
+        success: success,
+    });
+
+    if (success) return response.json();
+    else return [];
 };
